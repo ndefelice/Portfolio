187d39eb9a4dab4add1623e5b154fcf762b795cf
Nick DeFelice <ndefelice28@gmail.com>
2024-05-09 11:42:20 -0400

    Project card and page done

--- portfolio/app/components/project-card.tsx
+++ portfolio/app/components/project-card.tsx
@@ -1,29 +1,64 @@
-// // Create a card that displays an image as the background, with a tital and small description. 
-// // The card should be clickable and should navigate to the project page on GitHubwhen clicked.
-// 'use client'
+import React from 'react';
 
-// import React, { useRef } from 'react';
+interface ProjectCardProps {
+  title: string;
+  tags: string[];
+  description: string;
+  learnMoreLink: string;
+}
 
-// type ProjectCardProps = {
-//   title: string
-//   description: string
-//   image: string
-// }
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
 
-// export const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, image }) => {
-//   const cardRef = useRef<HTMLDivElement | null>(null);
-
-//   const handleClick = () => {
-//     window.location.href = "https://github.com/ndefelice/Spaceship-Titanic-ML";
-//   }
-
-//   return (
-//     <div className="card" ref={cardRef} onClick={handleClick}>
-//       <img src={image} alt={title} className="card-image" />
-//       <div className="w-full br-1">
-//         <h2 className="text-xl font-semibold">{title}</h2>
-//         <p className="card-description">{description}</p>
-//       </div>
-//     </div>
-//   )
-// }
+export default function ProjectCard({
+  title,
+  tags,
+  description,
+  learnMoreLink,
+}: ProjectCardProps) {
+  return (
+    <div className="flex flex-row border-b-4 border-b-zinc-800 py-10">
+      <div>
+        <h2 className="text-2xl font-semibold mb-6">{title}</h2>
+        <p className="text-md mb-8">{description}</p>
+        <div className="flex flex-wrap justify-start space-x-5">
+          {tags.map((tag) => (
+            <button
+              key={tag}
+              className="pl-3 pr-3 rounded-xl bg-slate-700 text-sm mb-2"
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+        <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+          <li>
+            <a
+              className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+              rel="noopener noreferrer"
+              target="_blank"
+              href={learnMoreLink}
+            >
+              <ArrowIcon />
+              <p className="ml-2 h-7">Learn More</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -1,5 +1,22 @@
 // import { BlogPosts } from 'app/components/posts'
-// import { ProjectCard } from "app/components/project-card"
+import ProjectCard from "app/components/project-card"
+
+// function ArrowIcon() {
+//   return (
+//     <svg
+//       width="12"
+//       height="12"
+//       viewBox="0 0 12 12"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <path
+//         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+//         fill="currentColor"
+//       />
+//     </svg>
+//   )
+// }
 
 export default function Page() {
   return (
@@ -7,11 +24,40 @@ export default function Page() {
       <h1 className="mb-8 text-3xl font-semibold tracking-tighter">
         Projects
       </h1>
-      {/* <ProjectCard
-        title="Project 1"
-        description="This is the first project"
-        image={require("https://miro.medium.com/v2/resize:fit:1080/1*OmoqdehwIUtvm2MakA5Xmw.jpeg")}
-      /> */}
+      <ProjectCard
+        title="Spaceship Titanic"
+        tags={["Python", "Seaborn", "Jupyter"]}
+        description="Using Python, I trained a support vector machine (SVM) model to predict transported passengers with 77.577% accuracy."
+        learnMoreLink="https://github.com/ndefelice/Spaceship-Titanic-ML"
+      />
     </section>
+    //   <div className="flex flex-row border-b-4 border-b-zinc-800 py-10">
+    //     <div>
+    //         <h2 className="text-xl font-semibold mb-5">"Spaceship Titanic" AI/ML Kaggle Competition</h2>
+    //         <div className="flex flex-row justify-start space-x-10 mb-5">
+    //             <button className="pl-4 pr-4 rounded-lg bg-slate-700">Python</button>
+    //             <button className="pl-4 pr-4 rounded-lg bg-slate-700">Seaborn</button>
+    //             <button className="pl-4 pr-4 rounded-lg bg-slate-700">Jupyter</button>
+    //         </div>
+    //         <p>
+    //           Using Python, I trained a support vector machine (SVM) model to predict transported 
+    //           passengers with 77.577% accuracy.
+    //         </p>
+    //         <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+    //           <li>
+    //             <a
+    //               className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+    //               rel="noopener noreferrer"
+    //               target="_blank"
+    //               href="https://github.com/ndefelice"
+    //             >
+    //               <ArrowIcon />
+    //               <p className="ml-2 h-7">Learn More</p>
+    //             </a>
+    //           </li>
+    //         </ul>
+    //     </div>
+    //   </div>
+    // </section>
   )
 }
